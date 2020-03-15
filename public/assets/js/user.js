74c3f628edb37246601f2a5c46ee40bd2766c3b6
lishifeng <10326849@qq.com>
2020-03-15 21:41:14 +0800

    添加用户功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -28,25 +28,56 @@ $('#avatar').on('change', function () {
     $.ajax({
         type: 'post',
         url: '/upload',
-        data:formData,    //上传数据
+        data: formData,    //上传数据
         //只要通过jQ中的ajax上传文件功能，就要设置
         processData: false,
         contentType: false,
         success: function (res) {
-           // console.log(res);  //返回带有图片地址的数组
-           $('#preImg').attr('src',res[0].avatar);// 找到索引为0  avatar的图片 替换掉(图片预览)
-        //将图片地址保存到表单中的某一个表单控件属性中
-           $('#hidden').val(res[0].avatar);
-   
-
+            // console.log(res);  //返回带有图片地址的数组
+            $('#preImg').attr('src', res[0].avatar);// 找到索引为0  avatar的图片 替换掉(图片预览)
+            //将图片地址保存到表单中的某一个表单控件属性中
+            $('#hidden').val(res[0].avatar);
         }
+    })
+})
+
+//完成用户添加功能（先给表单添加点击事件）
+$('#btnAdd').on('click',function(){
+    //serialize()收集表单数据（只对表单对象有用）
+    let data = $('form').serialize();
+     // console.log(data);  获取的是字符串
+      
+    $.ajax({
+      type:'post',
+      url:'/users',
+      data:data,
+       success:function(res){
+            //console.log(res); //响应回一个对象
+            userArr.push(res);//把res对象添加到userArr数组的最后面
+            render(); //防止刷新
+          //  数据添加到用户列表后，form表单数据应清空
+          $('input[type="email"]').val('');
+          $('input[name="nickName"]').val('');
+          $('input[name="password"]').val('');
+          $('#status0').prop('checked',false);   //将复选 设置为false 清空 复原
+          $('#status1').prop('checked',false);  //prop()检索属性值
+          $('#admin').prop('checked',false);
+          $('#normal').prop('checked',false);
+          $('#hidden').val('');  //隐藏域 里面的值清空
+          $('#preImg').attr('src','../assets/img/default.png');//把图像换回默认头像
 
 
+
+
+       },error:function(err){
+           console.log(err);
+           
+       }
 
     })
 
 
+
+
 })
-
-
 
