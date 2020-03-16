88e53307cf91a18a61a28ecfcda71622967a1c77
lishifeng <10326849@qq.com>
2020-03-16 17:07:48 +0800

    完成用户编辑功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -53,7 +53,7 @@ $('#btnAdd').on('click', function () {
         data: data,
         success: function (res) {
             //console.log(res); //响应回一个对象
-            userArr.push(res);//把res对象添加到userArr数组的最后面
+            userArr.unshift(res);//把res对象添加到userArr数组的最后面
             render(); //防止刷新
             //  数据添加到用户列表后，form表单数据应清空
             $('input[type="email"]').val('');
@@ -71,10 +71,12 @@ $('#btnAdd').on('click', function () {
         }
     })
 });
-
+var userId;//定义变量 接受id
 //给编辑添加注册点击事件，事件委托(将用户列表数据显示在左侧)
 $('tbody').on('click', '.edit', function () {
     //console.log('ok');
+    userId = $(this).attr('data-id')   //点击用户列表编辑获取 用户列表id
+
     $('h2').html('编辑用户'); // 将文字随着点击编辑而更换
     //获取当前被点击的元素的 父元素tr
     let tr = $(this).parents('tr');
@@ -83,8 +85,10 @@ $('tbody').on('click', '.edit', function () {
     $('#hidden').attr('src', tr.find('img').attr('src'));  //更换隐藏域中的值
 
     // console.log(tr.children().eq(2).text()); //用children([]).eq(index)方法 找到第三个id(邮箱)
-    $('input[name="email"]').val(tr.children().eq(2).text());
+    $('input[name="email"]').prop('disabled', true).val(tr.children().eq(2).text());
     $('input[name="nickName"]').val(tr.children().eq(3).text());
+    $('input[name="password"]').prop('disabled', true);// disabled禁用编辑
+
     //console.log(tr.children().eq(4).text());  打印出来是数字
     if (tr.children().eq(4).text() == '激活') {
         $('#status1').prop('checked', true)         //状态
@@ -97,8 +101,52 @@ $('tbody').on('click', '.edit', function () {
     } else {
         $('#normal').prop('checked', true)
     }
-  //将添加按钮显示， 编辑按钮隐藏
-  $('#btnAdd').hide();
-  $('#btnEdit').show();
+    //将添加按钮显示， 编辑按钮隐藏
+    $('#btnAdd').hide();
+    $('#btnEdit').show();
 })
+//完成编辑功能
+$('#btnEdit').on('click', function () {
+    //console.log(userId);  //点击左边编辑获取 右边用户列表id(字符串)
 
+    //收集表单数据
+    let data = $('form').serialize();
+
+    $.ajax({
+        type: 'PUT',
+        url: '/users/' + userId,
+        data: data,   //将右边的data(表单数据昵称) 赋给左
+        success: function (res) {
+            //console.log(res);  //通过接口拿到用户的id（对象）
+            //实现无刷新 编辑完，响应回一个对象，
+            // 页面上的数据保存到userArr数组
+            //从数组中将这元素的索引找到
+            let index = userArr.findIndex(item => res._id == item._id); //获取到两边的id  做比较 是否相等
+            userArr[index] = res;          //将响应回来的id 和用户修改的id替换     
+            //console.log(userArr);
+            render();
+
+            //编辑完成后，表单按钮以及文字要恢复成 添加用户     值要清空 （ 为了还原页面）
+            $('h2').html('添加新用户');//上面的文字替换
+            $('#preImg').attr('src', '../assets/img/default.png');
+            $('#hidden').val('');      //清空值
+            $('input[name="email"]').prop('disabled', false).val('');
+            $('input[name="nickName"]').val('');
+            $('input[name="password"]').prop('disabled', false);
+            //把复选框清空
+            $('#status0').prop('checked', false);
+            $('#status1').prop('checked', false);
+            $('#admin').prop('checked', false);
+            $('#normal').prop('checked', false);
+            //按钮替换
+            $('#btnAdd').show();  //添加用户显示
+            $('#btnEdit').hide(); //编辑用户 隐藏
+        },
+        error: function (err) {
+            console.log(err);
+
+        }
+
+    })
+
+})
